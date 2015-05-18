6346f405eb3fbb93829024fc6ec9812a0b74043c
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-18 17:02:13 -0400

    Change the build strategy

--- build.rs
+++ build.rs
@@ -10,25 +10,12 @@ fn main() {
     };
 
     if !env::var("CARGO_FEATURE_SYSTEM_OPENBLAS").is_ok() {
-        let cblas = !env::var("CARGO_FEATURE_EXCLUDE_CBLAS").is_ok();
+        let build = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("build");
+        let output = PathBuf::from(&env::var("OUT_DIR").unwrap());
 
-        let src = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("OpenBLAS");
-        let dst = PathBuf::from(&env::var("OUT_DIR").unwrap());
+        run(Command::new("make").current_dir(&build), "make");
 
-        env::remove_var("TARGET");
-
-        run(Command::new("make")
-                    .args(&["libs", "netlib", "shared"])
-                    .arg(&format!("{}_CBLAS=1", if cblas { "YES" } else { "NO" }))
-                    .arg(&format!("-j{}", env::var("NUM_JOBS").unwrap()))
-                    .current_dir(&src), "make");
-
-        run(Command::new("make")
-                    .arg("install")
-                    .arg(&format!("DESTDIR={}", dst.display()))
-                    .current_dir(&src), "make install");
-
-        println!("cargo:rustc-link-search={}", dst.join("opt/OpenBLAS/lib").display());
+        println!("cargo:rustc-link-search={}", output.join("opt/OpenBLAS/lib").display());
     }
 
     println!("cargo:rustc-link-lib={}=openblas", kind);
