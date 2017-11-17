b92f43f9228fd672c59dec47dd132e334c993c06
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-17 15:38:30 +0100

    Add a test for linking

---
+++ tests/lib.rs
@@ -0,0 +1,21 @@
+extern crate libc;
+extern crate openblas_src;
+
+use libc::c_float;
+
+extern {
+    pub fn srotg_(a: *mut c_float, b: *mut c_float, c: *mut c_float, s: *mut c_float);
+}
+
+#[test]
+fn link() {
+    unsafe {
+        let mut a: f32 = 0.0;
+        let mut b: f32 = 0.0;
+        let mut c: f32 = 42.0;
+        let mut d: f32 = 42.0;
+        srotg_(&mut a as *mut _, &mut b as *mut _, &mut c as *mut _, &mut d as *mut _);
+        assert!(c == 1.0);
+        assert!(d == 0.0);
+    }
+}
