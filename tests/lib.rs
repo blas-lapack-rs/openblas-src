aa27181fe1fc84a2fa64a423e2a793debf2d3f42
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-20 07:55:37 +0100

    Run rustfmt-nightly

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,7 +3,7 @@ extern crate openblas_src;
 
 use libc::c_float;
 
-extern {
+extern "C" {
     pub fn srotg_(a: *mut c_float, b: *mut c_float, c: *mut c_float, s: *mut c_float);
 }
 
@@ -14,7 +14,12 @@ fn link() {
         let mut b: f32 = 0.0;
         let mut c: f32 = 42.0;
         let mut d: f32 = 42.0;
-        srotg_(&mut a as *mut _, &mut b as *mut _, &mut c as *mut _, &mut d as *mut _);
+        srotg_(
+            &mut a as *mut _,
+            &mut b as *mut _,
+            &mut c as *mut _,
+            &mut d as *mut _,
+        );
         assert!(c == 1.0);
         assert!(d == 0.0);
     }
