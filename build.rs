ca1ebfac8eaddf69a5d7d1b266b3d313640bba0b
Ethan Smith <ethan@ethanhs.me>
2020-05-18 18:18:37 -0700

    Windows support through vcpkg

--- build.rs
+++ build.rs
@@ -17,48 +17,57 @@ fn main() {
     if !feature!("SYSTEM") {
         let cblas = feature!("CBLAS");
         let lapacke = feature!("LAPACKE");
-        let output = PathBuf::from(variable!("OUT_DIR").replace(r"\", "/"));
-        let mut make = Command::new("make");
-        make.args(&["libs", "netlib", "shared"])
-            .arg(format!("BINARY={}", binary!()))
-            .arg(format!("{}_CBLAS=1", switch!(cblas)))
-            .arg(format!("{}_LAPACKE=1", switch!(lapacke)))
-            .arg(format!("-j{}", variable!("NUM_JOBS")));
-        let target = match env::var("OPENBLAS_TARGET") {
-            Ok(target) => {
-                make.arg(format!("TARGET={}", target));
-                target
+        if cfg!(target_os="windows") {
+            if kind == "dynamic" {
+                env::set_var("VCPKGRS_DYNAMIC", "1");
+            } else {
+                env::set_var("CARGO_CFG_TARGET_FEATURE", "crt-static");
             }
-            _ => variable!("TARGET"),
-        };
-        env::remove_var("TARGET");
-        let source = if feature!("CACHE") {
-            PathBuf::from(format!("source_{}", target.to_lowercase()))
+            vcpkg::find_package("openblas").unwrap();
         } else {
-            output.join(format!("source_{}", target.to_lowercase()))
-        };
-        if !source.exists() {
-            let source_tmp = PathBuf::from(format!("{}_tmp", source.display()));
-            if source_tmp.exists() {
-                fs::remove_dir_all(&source_tmp).unwrap();
+            let output = PathBuf::from(variable!("OUT_DIR").replace(r"\", "/"));
+            let mut make = Command::new("make");
+            make.args(&["libs", "netlib", "shared"])
+                .arg(format!("BINARY={}", binary!()))
+                .arg(format!("{}_CBLAS=1", switch!(cblas)))
+                .arg(format!("{}_LAPACKE=1", switch!(lapacke)))
+                .arg(format!("-j{}", variable!("NUM_JOBS")));
+            let target = match env::var("OPENBLAS_TARGET") {
+                Ok(target) => {
+                    make.arg(format!("TARGET={}", target));
+                    target
+                }
+                _ => variable!("TARGET"),
+            };
+            env::remove_var("TARGET");
+            let source = if feature!("CACHE") {
+                PathBuf::from(format!("source_{}", target.to_lowercase()))
+            } else {
+                output.join(format!("source_{}", target.to_lowercase()))
+            };
+            if !source.exists() {
+                let source_tmp = PathBuf::from(format!("{}_tmp", source.display()));
+                if source_tmp.exists() {
+                    fs::remove_dir_all(&source_tmp).unwrap();
+                }
+                run(Command::new("cp").arg("-R").arg("source").arg(&source_tmp));
+                fs::rename(&source_tmp, &source).unwrap();
             }
-            run(Command::new("cp").arg("-R").arg("source").arg(&source_tmp));
-            fs::rename(&source_tmp, &source).unwrap();
-        }
-        for name in &vec!["CC", "FC", "HOSTCC"] {
-            if let Ok(value) = env::var(format!("OPENBLAS_{}", name)) {
-                make.arg(format!("{}={}", name, value));
+            for name in &vec!["CC", "FC", "HOSTCC"] {
+                if let Ok(value) = env::var(format!("OPENBLAS_{}", name)) {
+                    make.arg(format!("{}={}", name, value));
+                }
             }
+            run(&mut make.current_dir(&source));
+            run(Command::new("make")
+                .arg("install")
+                .arg(format!("DESTDIR={}", output.display()))
+                .current_dir(&source));
+            println!(
+                "cargo:rustc-link-search={}",
+                output.join("opt/OpenBLAS/lib").display(),
+            );
         }
-        run(&mut make.current_dir(&source));
-        run(Command::new("make")
-            .arg("install")
-            .arg(format!("DESTDIR={}", output.display()))
-            .current_dir(&source));
-        println!(
-            "cargo:rustc-link-search={}",
-            output.join("opt/OpenBLAS/lib").display(),
-        );
     }
     println!("cargo:rustc-link-lib=dylib=gfortran");
     println!("cargo:rustc-link-lib={}=openblas", kind);
