c2c0cd2218b0b945128b1a0b6470293ef0e91c5a
Ethan Smith <ethan@ethanhs.me>
2020-08-10 16:23:32 -0700

    Fix vcpkg usage

--- build.rs
+++ build.rs
@@ -15,58 +15,62 @@ fn main() {
         "dylib"
     };
     if !feature!("SYSTEM") {
+        if cfg!(target_os = "windows") {
+            panic!("Non-vcpkg builds are not supported on Windows (you must use the \"system\" feature.")
+        }
         let cblas = feature!("CBLAS");
         let lapacke = feature!("LAPACKE");
-        if cfg!(target_os="windows") {
+        let output = PathBuf::from(variable!("OUT_DIR").replace(r"\", "/"));
+        let mut make = Command::new("make");
+        make.args(&["libs", "netlib", "shared"])
+            .arg(format!("BINARY={}", binary!()))
+            .arg(format!("{}_CBLAS=1", switch!(cblas)))
+            .arg(format!("{}_LAPACKE=1", switch!(lapacke)))
+            .arg(format!("-j{}", variable!("NUM_JOBS")));
+        let target = match env::var("OPENBLAS_TARGET") {
+            Ok(target) => {
+                make.arg(format!("TARGET={}", target));
+                target
+            }
+            _ => variable!("TARGET"),
+        };
+        env::remove_var("TARGET");
+        let source = if feature!("CACHE") {
+            PathBuf::from(format!("source_{}", target.to_lowercase()))
+        } else {
+            output.join(format!("source_{}", target.to_lowercase()))
+        };
+        if !source.exists() {
+            let source_tmp = PathBuf::from(format!("{}_tmp", source.display()));
+            if source_tmp.exists() {
+                fs::remove_dir_all(&source_tmp).unwrap();
+            }
+            run(Command::new("cp").arg("-R").arg("source").arg(&source_tmp));
+            fs::rename(&source_tmp, &source).unwrap();
+        }
+        for name in &vec!["CC", "FC", "HOSTCC"] {
+            if let Ok(value) = env::var(format!("OPENBLAS_{}", name)) {
+                make.arg(format!("{}={}", name, value));
+            }
+        }
+        run(&mut make.current_dir(&source));
+        run(Command::new("make")
+            .arg("install")
+            .arg(format!("DESTDIR={}", output.display()))
+            .current_dir(&source));
+        println!(
+            "cargo:rustc-link-search={}",
+            output.join("opt/OpenBLAS/lib").display(),
+        );
+    } else {
+        if cfg!(target_os = "windows") {
             if kind == "dylib" {
                 env::set_var("VCPKGRS_DYNAMIC", "1");
             } else {
                 env::set_var("CARGO_CFG_TARGET_FEATURE", "crt-static");
             }
+            #[cfg(target_os = "windows")]
             vcpkg::find_package("openblas").unwrap();
-        } else {
-            let output = PathBuf::from(variable!("OUT_DIR").replace(r"\", "/"));
-            let mut make = Command::new("make");
-            make.args(&["libs", "netlib", "shared"])
-                .arg(format!("BINARY={}", binary!()))
-                .arg(format!("{}_CBLAS=1", switch!(cblas)))
-                .arg(format!("{}_LAPACKE=1", switch!(lapacke)))
-                .arg(format!("-j{}", variable!("NUM_JOBS")));
-            let target = match env::var("OPENBLAS_TARGET") {
-                Ok(target) => {
-                    make.arg(format!("TARGET={}", target));
-                    target
-                }
-                _ => variable!("TARGET"),
-            };
-            env::remove_var("TARGET");
-            let source = if feature!("CACHE") {
-                PathBuf::from(format!("source_{}", target.to_lowercase()))
-            } else {
-                output.join(format!("source_{}", target.to_lowercase()))
-            };
-            if !source.exists() {
-                let source_tmp = PathBuf::from(format!("{}_tmp", source.display()));
-                if source_tmp.exists() {
-                    fs::remove_dir_all(&source_tmp).unwrap();
-                }
-                run(Command::new("cp").arg("-R").arg("source").arg(&source_tmp));
-                fs::rename(&source_tmp, &source).unwrap();
-            }
-            for name in &vec!["CC", "FC", "HOSTCC"] {
-                if let Ok(value) = env::var(format!("OPENBLAS_{}", name)) {
-                    make.arg(format!("{}={}", name, value));
-                }
-            }
-            run(&mut make.current_dir(&source));
-            run(Command::new("make")
-                .arg("install")
-                .arg(format!("DESTDIR={}", output.display()))
-                .current_dir(&source));
-            println!(
-                "cargo:rustc-link-search={}",
-                output.join("opt/OpenBLAS/lib").display(),
-            );
         }
     }
     println!("cargo:rustc-link-lib={}=openblas", kind);
