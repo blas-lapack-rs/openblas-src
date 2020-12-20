14638a0b31773e7fa5548f50de600b598382d2dc
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-12-21 02:00:03 +0900

    Split build script as `naive_build`

--- openblas-src/build.rs
+++ openblas-src/build.rs
@@ -94,72 +94,7 @@ fn main() {
                 "Non-vcpkg builds are not supported on Windows. You must use the 'system' feature."
             )
         }
-
-        let output = PathBuf::from(env::var("OUT_DIR").unwrap().replace(r"\", "/"));
-        let mut make = Command::new("make");
-        make.args(&["libs", "netlib", "shared"])
-            .arg(format!("BINARY={}", binary()))
-            .arg(format!(
-                "{}_CBLAS=1",
-                if feature_enabled("cblas") {
-                    "YES"
-                } else {
-                    "NO"
-                }
-            ))
-            .arg(format!(
-                "{}_LAPACKE=1",
-                if feature_enabled("lapacke") {
-                    "YES"
-                } else {
-                    "NO"
-                }
-            ));
-        match env::var("OPENBLAS_ARGS") {
-            Ok(args) => {
-                make.args(args.split_whitespace());
-            }
-            _ => (),
-        };
-        if let Ok(num_jobs) = env::var("NUM_JOBS") {
-            make.arg(format!("-j{}", num_jobs));
-        }
-        let target = match env::var("OPENBLAS_TARGET") {
-            Ok(target) => {
-                make.arg(format!("TARGET={}", target));
-                target
-            }
-            _ => env::var("TARGET").unwrap(),
-        };
-        env::remove_var("TARGET");
-        let source = if feature_enabled("cache") {
-            PathBuf::from(format!("source_{}", target.to_lowercase()))
-        } else {
-            output.join(format!("source_{}", target.to_lowercase()))
-        };
-
-        if !source.exists() {
-            let source_tmp = PathBuf::from(format!("{}_tmp", source.display()));
-            if source_tmp.exists() {
-                fs::remove_dir_all(&source_tmp).unwrap();
-            }
-            run(Command::new("cp").arg("-R").arg("source").arg(&source_tmp));
-            fs::rename(&source_tmp, &source).unwrap();
-        }
-        for name in &vec!["CC", "FC", "HOSTCC"] {
-            if let Ok(value) = env::var(format!("OPENBLAS_{}", name)) {
-                make.arg(format!("{}={}", name, value));
-            }
-        }
-        run(&mut make.current_dir(&source));
-        run(Command::new("make")
-            .arg("install")
-            .arg(format!("DESTDIR={}", output.display()))
-            .current_dir(&source));
-        println!(
-            "cargo:rustc-link-search={}",
-            output.join("opt/OpenBLAS/lib").display(),
-        );
+        naive_build();
     }
     println!("cargo:rustc-link-lib={}=openblas", link_kind);
 }
@@ -176,4 +111,73 @@ fn run(command: &mut Command) {
             panic!("Failed: `{:?}` ({})", command, error);
         }
     }
+}
+
+/// openblas-src 0.9.0 compatible `make` runner
+fn naive_build() {
+    let output = PathBuf::from(env::var("OUT_DIR").unwrap().replace(r"\", "/"));
+    let mut make = Command::new("make");
+    make.args(&["libs", "netlib", "shared"])
+        .arg(format!("BINARY={}", binary()))
+        .arg(format!(
+            "{}_CBLAS=1",
+            if feature_enabled("cblas") {
+                "YES"
+            } else {
+                "NO"
+            }
+        ))
+        .arg(format!(
+            "{}_LAPACKE=1",
+            if feature_enabled("lapacke") {
+                "YES"
+            } else {
+                "NO"
+            }
+        ));
+    match env::var("OPENBLAS_ARGS") {
+        Ok(args) => {
+            make.args(args.split_whitespace());
+        }
+        _ => (),
+    };
+    if let Ok(num_jobs) = env::var("NUM_JOBS") {
+        make.arg(format!("-j{}", num_jobs));
+    }
+    let target = match env::var("OPENBLAS_TARGET") {
+        Ok(target) => {
+            make.arg(format!("TARGET={}", target));
+            target
+        }
+        _ => env::var("TARGET").unwrap(),
+    };
+    env::remove_var("TARGET");
+    let source = if feature_enabled("cache") {
+        PathBuf::from(format!("source_{}", target.to_lowercase()))
+    } else {
+        output.join(format!("source_{}", target.to_lowercase()))
+    };
+
+    if !source.exists() {
+        let source_tmp = PathBuf::from(format!("{}_tmp", source.display()));
+        if source_tmp.exists() {
+            fs::remove_dir_all(&source_tmp).unwrap();
+        }
+        run(Command::new("cp").arg("-R").arg("source").arg(&source_tmp));
+        fs::rename(&source_tmp, &source).unwrap();
+    }
+    for name in &vec!["CC", "FC", "HOSTCC"] {
+        if let Ok(value) = env::var(format!("OPENBLAS_{}", name)) {
+            make.arg(format!("{}={}", name, value));
+        }
+    }
+    run(&mut make.current_dir(&source));
+    run(Command::new("make")
+        .arg("install")
+        .arg(format!("DESTDIR={}", output.display()))
+        .current_dir(&source));
+    println!(
+        "cargo:rustc-link-search={}",
+        output.join("opt/OpenBLAS/lib").display(),
+    );
 }
