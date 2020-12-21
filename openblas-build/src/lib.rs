e031f24359719fafd257f57710c4e20146658ff6
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-12-22 02:42:49 +0900

    Rewrite anyhow to thiserror

--- openblas-build/src/lib.rs
+++ openblas-build/src/lib.rs
@@ -14,32 +14,6 @@
 
 mod build;
 mod check;
+pub mod error;
 pub use build::*;
 pub use check::*;
-
-use anyhow::{bail, Result};
-use std::process::Command;
-
-pub(crate) trait CheckCall {
-    fn check_call(&mut self) -> Result<()>;
-}
-
-impl CheckCall for Command {
-    fn check_call(&mut self) -> Result<()> {
-        match self.status() {
-            Ok(status) => {
-                if !status.success() {
-                    bail!(
-                        "Subprocess returns with non-zero status: `{:?}` ({})",
-                        self,
-                        status
-                    );
-                }
-                Ok(())
-            }
-            Err(error) => {
-                bail!("Subprocess execution failed: `{:?}` ({})", self, error);
-            }
-        }
-    }
-}
