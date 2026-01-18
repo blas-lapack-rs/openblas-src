6e4a4c3a02cb0315c86753bd8aa1c431a76c8566
Dirreke <mingyang_ge@163.com>
2026-01-19 01:44:46 +0800

    fix doc

--- openblas-src/src/lib.rs
+++ openblas-src/src/lib.rs
@@ -1,2 +1,3 @@
-#![doc = include_str!("../../README.md")]
+// Use `CARGO_PKG_README` because of https://github.com/rust-lang/cargo/issues/11597
+#![doc = include_str!(concat!("../", env!("CARGO_PKG_README")))]
 #![no_std]
